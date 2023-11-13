e0f2f634576dfd4140a8d3a496cfc3c6e34ccdab
WILLIAM SÁNCHEZ <william_as@tesch.edu.mx>
2023-11-13 11:19:34 -0600

    Mejore código de inicio de sesión

--- app/src/main/java/com/example/quetzalli/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/quetzalli/data/repository/UserRepository.kt
@@ -1,10 +1,7 @@
 package com.example.quetzalli.data.repository
 
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
-import com.google.android.gms.tasks.Tasks
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
@@ -15,10 +12,9 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class UserRepository @Inject constructor(private val auth: FirebaseAuth, db : FirebaseFirestore){
-    suspend fun handleSignInResult(completedTask: Task<GoogleSignInAccount>): FetchResult<FirebaseUser> {
+    suspend fun loginWithGoogle(idToken: String): FetchResult<FirebaseUser> {
         return try {
-            val account = completedTask.getResult(ApiException::class.java)
-            val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+            val credential = GoogleAuthProvider.getCredential(idToken, null)
             val authResult = auth.signInWithCredential(credential).await()
             val user = authResult.user
             if (user != null) {
@@ -27,10 +23,8 @@ class UserRepository @Inject constructor(private val auth: FirebaseAuth, db : Fi
                 FetchResult.Error(Exception("User is null"))
             }
         } catch (e: ApiException) {
-            // Maneja específicamente las ApiException
             FetchResult.Error(e)
         } catch (e: Exception) {
-            // Maneja todas las demás excepciones
             FetchResult.Error(e)
         }
     }
--- app/src/main/java/com/example/quetzalli/viewmodel/UserVM.kt
+++ app/src/main/java/com/example/quetzalli/viewmodel/UserVM.kt
@@ -1,48 +1,31 @@
 package com.example.quetzalli.viewmodel
 
-import android.app.Activity
-import android.content.Intent
-import androidx.activity.result.ActivityResultLauncher
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.quetzalli.R
 import com.example.quetzalli.data.repository.FetchResult
 import com.example.quetzalli.data.repository.UserRepository
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseUser
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class UserVM @Inject constructor(private val repository: UserRepository) : ViewModel() {
+class UserVM @Inject constructor(private val userRepo: UserRepository) : ViewModel() {
 
-    private lateinit var googleSignInClient: GoogleSignInClient
+    // LiveData to observe the user
+    private val _user = MutableLiveData<FirebaseUser?>()
+    val user: MutableLiveData<FirebaseUser?> get() = _user
 
-    fun initGoogleSignInClient(activity: Activity) {
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(activity.getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
-
-        googleSignInClient = GoogleSignIn.getClient(activity, gso)
-    }
-
-    val loginResult = MutableLiveData<FetchResult<FirebaseUser>>()
-
-    fun loginWithGoogle(launcher: ActivityResultLauncher<Intent>) {
-        val signInIntent = googleSignInClient.signInIntent
-        launcher.launch(signInIntent)
-    }
-
-    fun handleSignInResult(data: Intent?) {
-        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+    // Function to login with Google
+    fun loginWithGoogle(idToken: String) {
         viewModelScope.launch {
-            val result = repository.handleSignInResult(task)
-            loginResult.value = result
+            when (val result = userRepo.loginWithGoogle(idToken)) {
+                is FetchResult.Success -> _user.value = result.data
+                is FetchResult.Error -> {
+                    // Handle error
+                }
+            }
         }
     }
 }
