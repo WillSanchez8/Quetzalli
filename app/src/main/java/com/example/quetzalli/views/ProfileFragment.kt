c9b03f076217d9b3480602936be9d63fac2928d8
WILLIAM SÁNCHEZ <william_as@tesch.edu.mx>
2023-11-20 20:24:19 -0600

    conexión de vistas de Fernando y Josué, se agregaron dialogos, aun falta funcionalidad

--- app/src/main/java/com/example/quetzalli/views/ProfileFragment.kt
+++ app/src/main/java/com/example/quetzalli/views/ProfileFragment.kt
@@ -5,56 +5,84 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.example.quetzalli.R
+import com.example.quetzalli.databinding.FragmentProfileBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var binding: FragmentProfileBinding
+    private lateinit var navController: NavController
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding= FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
     }
+
+    private fun init(view: View) {
+        navController = findNavController()
+
+        binding.btnCambio.setOnClickListener {
+            navController.navigate(R.id.action_perfil_to_dataPersonalFragment)
+        }
+
+        binding.btnPoliticas.setOnClickListener {
+            navController.navigate(R.id.action_perfil_to_policyFragment)
+        }
+
+        binding.btnAyuda.setOnClickListener {
+            navController.navigate(R.id.action_perfil_to_helpFragment)
+        }
+
+        binding.btnComentarios.setOnClickListener {
+            val input = TextInputEditText(view.context).apply {
+                hint = resources.getString(R.string.hint_comentarios)
+                setPadding(20, 10, 20, 10)
+            }
+            val inputLayout = TextInputLayout(view.context).apply {
+                boxBackgroundMode = TextInputLayout.BOX_BACKGROUND_OUTLINE
+                addView(input)
+            }
+            MaterialAlertDialogBuilder(view.context)
+                .setTitle(resources.getString(R.string.comentarios))
+                .setMessage(resources.getString(R.string.comentarios_message))
+                .setView(inputLayout)
+                .setPositiveButton(resources.getString(R.string.accept)) { dialog, which ->
+                    val comentario = input.text.toString()
+                    // Aquí puedes manejar el comentario del usuario
+                    dialog.dismiss()
+                }
+                .setNegativeButton(resources.getString(R.string.decline)) { dialog, which ->
+                    dialog.dismiss()
+                }
+                .show()
+        }
+
+        binding.btnEliminar.setOnClickListener {
+            MaterialAlertDialogBuilder(view.context)
+                .setTitle(resources.getString(R.string.delete_account))
+                .setMessage(resources.getString(R.string.delete_account_message))
+                .setPositiveButton(resources.getString(R.string.delete)) { dialog, which ->
+                    dialog.dismiss()
+                }
+                .setNegativeButton(resources.getString(R.string.decline)) { dialog, which ->
+                    dialog.dismiss()
+                }
+                .show()
+        }
+    }
+
 }
