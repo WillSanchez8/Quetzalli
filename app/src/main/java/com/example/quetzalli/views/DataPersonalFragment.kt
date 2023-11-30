ca9f23e8462ffb86ed73fb0f3b5b447d755aaa96
FernandoRV98 <128876803+FernandoRV98@users.noreply.github.com>
2023-11-30 11:13:28 -0600

    Solo falta obtener foto...

--- app/src/main/java/com/example/quetzalli/views/DataPersonalFragment.kt
+++ app/src/main/java/com/example/quetzalli/views/DataPersonalFragment.kt
@@ -13,11 +13,13 @@ import com.example.quetzalli.databinding.FragmentDataPersonalBinding
  // VIEW MODEL
 import com.example.quetzalli.viewmodel.UserVM
 import androidx.fragment.app.viewModels
+import com.example.quetzalli.data.repository.FetchResult
+import com.example.quetzalli.data.models.User
 
 class DataPersonalFragment : Fragment() {
     private val userVM: UserVM by viewModels()
-
     private lateinit var binding: FragmentDataPersonalBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -47,12 +49,33 @@ class DataPersonalFragment : Fragment() {
 
     // Function to load user data into the form
     private fun loadUserData() {
-        userVM.user.observe(viewLifecycleOwner) { user ->
-            if (user != null) {
-                binding.etName.setText(user.displayName)
+        userVM.getUserById(userVM.getCurrentUser()?.uid?:"").observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is FetchResult.Success -> {
+                    // name
+                    binding.etName.setText(result.data?.name).toString()
+                    // occupation
+                    binding.etOcupation.setText(result.data?.occupation).toString()
+                    // date
+                    binding.etDate.setText(result.data?.date).toString()
+                    // marcar RadioButton de genere
+                    val gene = result.data?.gender.toString()
+                    if (gene == "Femenino") {
+                        binding.rdFemale.isChecked = true
+                    }
+                    if (gene == "Masculino") {
+                        binding.rdMale.isChecked = true
+                    } else {
+                        binding.other.isChecked = true
+                    }
+                }
+                is FetchResult.Error -> {
+                    // Maneja el error aquí
+                }
             }
         }
     }
+
 
 
 }
