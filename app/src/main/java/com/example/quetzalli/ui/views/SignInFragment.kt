6a771d6ecf6aa0f09a58b257ea6f0773be65a58b
William Aguilar <william@touchesbegan.com>
2024-06-23 15:05:28 -0600

    se agrego verificacion de conexion

--- app/src/main/java/com/example/quetzalli/ui/views/SignInFragment.kt
+++ app/src/main/java/com/example/quetzalli/ui/views/SignInFragment.kt
@@ -2,6 +2,7 @@ package com.example.quetzalli.ui.views
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -106,6 +107,7 @@ class SignInFragment : Fragment() {
                 }
             } catch (e: ApiException) {
                 // Google Sign In failed
+                Log.e("No CONNECTION", "$e")
                 Snackbar.make(
                     binding.root,
                     "Error al iniciar sesión, intente de nuevo más tarde",
